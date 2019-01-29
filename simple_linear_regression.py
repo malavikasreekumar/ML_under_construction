65fbd86d7b93d19caafcc99af68cf6daba0bd598
Malavika Sreekumar <35482521+malavikasreekumar@users.noreply.github.com>
2019-01-29 17:23:10 +0530

    Visualizing the test and training set result

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -11,8 +11,8 @@ X=dataset.iloc[:, :-1].values
 y=dataset.iloc[:, ].values
 
 # Splitting test set and training set
-from sklearn.cross_validation import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=1/3,train_size=2/3,random_state=0)#the train size and test size tells us the part of dataset we take for each
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=1/3,random_state=0)#the train size and test size tells us the part of dataset we take for each
 
 
 #feature scaling
@@ -25,5 +25,21 @@ X_test=sc_X.fit_transform(X_test)"""
 from sklearn.linear_model import LinearRegression
 regressor= LinearRegression()
 regressor.fit(X_train,y_train)
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
 
-y_pred=regressor.predict(X_test)
+# Visualising the Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
