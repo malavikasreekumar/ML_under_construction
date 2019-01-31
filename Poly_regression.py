a2e4c5a7611d6fca2538e825721e1e0b351d55c6
malavikasreekumar <malavika15jan@gmail.com>
2019-01-31 11:28:24 +0530

    First commit

--- Poly_regression.py
+++ Poly_regression.py
@@ -25,3 +25,23 @@ poly_reg=PolynomialFeatures(degree=4)
 X_poly=poly_reg.fit_transform(X)
 lin_reg_2=LinearRegression()
 lin_reg_2.fit(X_poly,y)
+
+#Visualising the Linear Regression result
+plt.scatter(X,y,color='red')
+plt.plot(X,lin_reg.predict(X),color='blue')
+plt.title('Truth or Bluff(Linear Regression)')
+plt.xlabel('Positional level')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Polynomial Regression result
+X_grid=np.arange(min(X),max(X),0.1)#This will give us a better curve ie higher resolution
+X_grid=X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color='red')
+plt.plot(X_grid,lin_reg_2.predict(poly_reg.fit_transform(X_grid)),color='blue')
+plt.title('Truth or Bluff(Polynomial Regression)')
+plt.xlabel('Positional level')
+plt.ylabel('Salary')
+plt.show()
+
+ 
