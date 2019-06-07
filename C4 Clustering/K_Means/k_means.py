1e4e9b0f8ac7225985a5d7434e6e163228a1fa0a
Malavika Sreekumar <35482521+malavikasreekumar@users.noreply.github.com>
2019-06-07 16:11:28 +0530

    Update k_means.py

--- C4 Clustering/K_Means/k_means.py
+++ C4 Clustering/K_Means/k_means.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
+
 """
 Created on Fri Jun  7 11:50:38 2019
 
 @author: Malavika  Sreekumar
 """
 
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
